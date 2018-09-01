0b8cee85811300acc71973fb46adee42e0c217d4
Graham Gilpatric <grahamwithag@gmail.com>
2018-09-01 19:22:32 -0400

    api get

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,22 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const pgp = require('pg-promise')();
+const db = pgp(process.env.DATABASE_URL || 'postgres://localhost:5432/todo');
+const path = require('path');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+router.get('/api/v1/items', function(req, res, next) {
+  const results = db.any('SELECT * FROM items', [true])
+    .then(function(data) {
+      res.send(data)
+    })
+    .catch(function(error) {
+        console.error(error);
+    });
+})
 
 module.exports = router;
