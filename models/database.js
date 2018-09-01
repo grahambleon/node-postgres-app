949255202953dabfb4de9db483e5da812044203e
Graham Gilpatric <grahamwithag@gmail.com>
2018-09-01 17:07:27 -0400

    create items table

--- models/database.js
+++ models/database.js
@@ -1,8 +1,10 @@
-const pg = require('pg');
-const connectionString = process.env.DATABASE_URL || 'postgres://localhost:5432/todo';
+const pgp = require('pg-promise')();
+const db = pgp(process.env.DATABASE_URL || 'postgres://localhost:5432/todo');
 
-const client = new pg.Client(connectionString);
-client.connect();
-const query = client.query(
-  'CREATE TABLE items(id SERIAL PRIMARY KEY, text VARCHAR(40) not null, complete BOOLEAN)');
-query.on('end', () => { client.end(); });
+db.any('CREATE TABLE items(id SERIAL PRIMARY KEY, text VARCHAR(40) not null, complete BOOLEAN)', [true])
+    .then(data => {
+        console.log('DATA:', data); // print data;
+    })
+    .catch(error => {
+        console.log('ERROR:', error); // print the error;
+    })
