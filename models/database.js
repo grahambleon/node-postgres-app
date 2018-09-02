ef64d945f096d145ce56df1be33e0eee44a8d8b7
Graham Gilpatric <grahamwithag@gmail.com>
2018-09-01 21:49:24 -0400

    updated syntax

--- models/database.js
+++ models/database.js
@@ -2,9 +2,10 @@ const pgp = require('pg-promise')();
 const db = pgp(process.env.DATABASE_URL || 'postgres://localhost:5432/todo');
 
 db.none(
-  'CREATE TABLE items (id char(5) CONSTRAINT firstkey PRIMARY KEY, text varchar(40) NOT NULL, complete BOOLEAN);', [true])
+  'CREATE TABLE items (id SERIAL PRIMARY KEY, text varchar(40) NOT NULL, complete BOOLEAN);', [true])
   .then(() => {
     console.log('Success! :D');
+    pgp.end();
   })
   .catch(error => {
     console.error(error);
--- routes/index.js
+++ routes/index.js
@@ -5,20 +5,30 @@ const db = pgp(process.env.DATABASE_URL || 'postgres://localhost:5432/todo');
 const path = require('path');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', (req, res) => {
   res.render('index', { title: 'Express' });
 });
 
-router.get('/api/v1/items', function(req, res, next) {
+router.get('/api/v1/items', (req, res) => {
   db.any('SELECT * FROM items', [true])
-  .then(function(data) {
-    res.send(data)
-  })
-  .catch(function(error) {
-    console.error(error);
-  });
+  .then(data => res.send(data))
+  .catch(error => console.error(error));
 })
 
-// router.post('api/v1/items')
+router.post('api/v1/items', (req, res) => {
+  // const data = {
+  //   text: req.body.text,
+  //   complete: false
+  // }
+  // db.none(`INSERT INTO items VALUES(DEFAULT, ${data.text}, ${data.complete}`), [true])
+  //   .then(() => {
+  //     console.log('SUCCESS :D');
+  //   })
+  //   .catch(error => {
+  //     console.error(error);
+  //   });
+  console.log(req.body);
+  res.end();
+})
 
 module.exports = router;
